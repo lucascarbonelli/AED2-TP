7602bcee424c0226d67be170dbe3ab29d44dbfd8
istng <ivanstng@mail.com>
2016-10-05 15:37:24 -0300

    estructuras casi definitivas en actualizado

--- estrTP2actualizado.groovy
+++ estrTP2actualizado.groovy
@@ -1,8 +1,11 @@
-conj(jugadores) => vector(tupla<id, tupla<estaConect, sanciones, posicion, vector(tupla<tipoPokemon, cantidad>)>>)
-
+conj(jugadores) => vector(tupla<id, tupla<estaConect, sanciones, posicion, conjuntoLineal(tupla<tipoPokemon, cantidad>), it(a id en heap)>>)
+																			//con it							//este puntero lleva al heap
+																												//del poke cercano
 conj(expulsados) => vector(id)
 								//este cant es de poke de ese tipo
-conj(Pokemons) => trie(tipo, tupla<cant, vector(tupla<coord, cantMovCapt, heap(id)>)>)) 
+conj(Pokemons) => diccTrie(tipo, tupla<cant, conjuntoLineal(tupla<coord, cantMovCapt, heap(id)>)>)) 
 						//representa todos los pokes: 			//el heap debe estar
-						//si no hay salvajes, su vector			//ordenado por la
+						//si no hay salvajes, su conjuntoLineal	//ordenado por la
 						//de coord está vacío					//cant de pokes de las id
+
+posPokemonesCercano => matriz(coord)
