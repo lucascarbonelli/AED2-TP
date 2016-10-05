5b200ef21661b7d076ecc7271365765f44f78825
istng <ivanstng@mail.com>
2016-10-05 15:51:52 -0300

    actualizado re papurri la estttrrrtrtrttt

--- estrTP2actualizado.groovy
+++ estrTP2actualizado.groovy
@@ -2,10 +2,17 @@ conj(jugadores) => vector(tupla<id, tupla<estaConect, sanciones, posicion, conju
 																			//con it							//este puntero lleva al heap
 																												//del poke cercano
 conj(expulsados) => vector(id)
-								//este cant es de poke de ese tipo
+
+conj(Pokemons) => diccTrie(tipo, it)
+							//it a la tupla correcta del conj de abajo
+posConPokes => conjuntoLineal(tupla<cant, conjuntoLineal(tupla<coord, cantMovCapt, heap(id)>)>)
+									//cant total de pokes (ps y pc), si el tipo es solo pc, su conj es vacio
+posPokemonesCercano => matriz(coord)
+
+
+/*
 conj(Pokemons) => diccTrie(tipo, tupla<cant, conjuntoLineal(tupla<coord, cantMovCapt, heap(id)>)>)) 
 						//representa todos los pokes: 			//el heap debe estar
 						//si no hay salvajes, su conjuntoLineal	//ordenado por la
 						//de coord está vacío					//cant de pokes de las id
-
-posPokemonesCercano => matriz(coord)
+*/
