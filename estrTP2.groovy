0ac7f93512c5316567f3b0517026d4df264ab649
istng <ivanstng@mail.com>
2016-10-05 14:20:27 -0300

    agrego el groovy de las estr

---
+++ estrTP2.groovy
@@ -0,0 +1,7 @@
+pokemonesSalvajes -> <tam!,trie(string,<tam!,conjLineal(infoPoke))>>
+infoPoke -> <coord,JugadoresEnRango:ColaPrioridad(<#PCj,j>)>  
+Jugadores -> Array???????????(infoJug)
+infoJug -> <conectado?, sanciones, MultiConj(pokemons)???????,itCola???????>
+mapa->mapa:laViejaDeIvan
+coord -> <latitud:nat,longitud:nat>
+putoelquelee.()
